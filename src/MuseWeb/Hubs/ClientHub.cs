3e1a66700c4bd655c5f075314ce53aab426ab97d
krisbbb <krisbbb@gmail.com>
2018-05-17 11:19:45 -0700

    Initial client with basic input and some debug display

---
+++ src/MuseWeb/Hubs/ClientHub.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace MuseWeb.Hubs
+{
+    [Authorize]
+    public class ClientHub : Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("SendAction", Context.User.Identity.Name, "joined");
+        }
+
+        public override async Task OnDisconnectedAsync(Exception ex)
+        {
+            await Clients.All.SendAsync("SendAction", Context.User.Identity.Name, "left");
+        }
+
+        // public async Task Send(string message)
+        // {
+        //     await Clients.All.SendAsync("SendMessage", Context.User.Identity.Name, message);
+        // }
+
+        public static int x = 0;
+        public static int y = 0;
+
+        public async Task UserCommand(string command)
+        {
+            Console.WriteLine(String.Format("got command ({0})", command));
+            switch(command)
+            {
+                case "north":
+                    y = Math.Max(0, y - 1);
+                break;
+                case "south":
+                    y = Math.Min(8, y + 1);
+                break;
+                case "east":
+                    x = Math.Min(8, x + 1);
+                break;
+                case "west":
+                    x = Math.Max(0, x - 1);
+                break;
+                default:
+                break;
+            }
+
+            await Clients.All.SendAsync("shapeMoved", x, y);
+        }
+    }
+}
