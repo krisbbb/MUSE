ea5e4a7ac0180764517ee6cb2e3c84867dd6d534
krisbbb <krisbbb@gmail.com>
2018-06-12 08:32:41 -0700

    Basic movement of player sprite

--- src/MuseWeb/wwwroot/js/example/junk.js
+++ src/MuseWeb/wwwroot/js/example/junk.js
@@ -1,103 +1,86 @@
 
 console.log("this is the junk test example.")
-console.log("Another log message.")
-
 
 window.addEventListener('load',function(e) {
 
   var connection = new signalR.HubConnection("/hubs/client");
 
+  // Set up a standard Quintus instance with only the 
+  // Sprites and Scene module (for the stage support) loaded.
+  var Q = window.Q = Quintus().include("Sprites, Scenes, 2D, Input")
+                            .setup({ width: 1000, height: 600 });
 
+  Q.options.imagePath = '/images/';
+  Q.gravityY = 0;
 
-    // Set up a standard Quintus instance with only the 
-    // Sprites and Scene module (for the stage support) loaded.
-    var Q = window.Q = Quintus().include("Sprites, Scenes, 2D, Input")
-                                .setup({ width: 1000, height: 600 });
+  // Create a simple scene that adds two shapes on the page
+  Q.scene("start",function(stage) {
 
-    Q.options.imagePath = '/images/';
-    Q.gravityY = 0;
-  
-    // // Draw vertical lines at every 100 pixels for visual indicators
-    // function drawLines(ctx) {
-    //   ctx.save();
-    //   ctx.strokeStyle = '#FFFFFF';
-    //   for(var x = 0;x < 1000;x+=100) {
-    //     ctx.beginPath();
-    //     ctx.moveTo(x,0);
-    //     ctx.lineTo(x,600);
-    //     ctx.stroke();
-    //   }
-    //   ctx.restore();
-    // }
-  
-    // Create a simple scene that adds two shapes on the page
-    Q.scene("start",function(stage) {
-  
-      // A basic sprite shape a asset as the image
-      var sprite1 = new Q.Sprite({ x: 500, y: 100, asset: 'enemy.png', 
-                                   angle: 0, collisionMask: 1, scale: 1});
-      sprite1.p.points = [
-        [ -150, -120 ],
-        [  150, -120 ],
-        [  150,   60 ],
-        [   90,  120 ],
-        [  -90,  120 ],
-        [ -150,   60 ]
-        ];
-      stage.insert(sprite1);
-      // Add the 2D component for collision detection and gravity.
-      sprite1.add('2d')
-  
-      sprite1.on('step',function() {
-  
-      });
-  
-      // A red platform for the other sprite to land on
-      var sprite2 = new Q.Sprite({ x: 500, y: 600, w: 300, h: 200 });
-      sprite2.draw= function(ctx) {
-        ctx.fillStyle = '#FF0000';
-        ctx.fillRect(-this.p.cx,-this.p.cy,this.p.w,this.p.h);
-      };
-      stage.insert(sprite2);
-      // Bind the basic inputs to different behaviors of sprite1
-      Q.input.on('up',stage,function(e) { 
-        //sprite1.p.scale -= 0.1;
-        connection.send("UserCommand", "north"); // keycode 38:up arrow
-      });
-  
-      Q.input.on('down',stage,function(e) { 
-        //sprite1.p.scale += 0.1;
-        connection.send("UserCommand", "south"); // keycode 40:down arrow
-      });
-  
-      Q.input.on('left',stage,function(e) {
-        //sprite1.p.angle -= 5;
-        connection.send("UserCommand", "west"); // keycode 37:left arrow
-      });
-  
-      Q.input.on('right',stage,function(e) {
-        //sprite1.p.angle += 5;
-        connection.send("UserCommand", "east"); // keycode 39:right arrow
-      });
-  
-      Q.input.on('fire',stage,function(e) {
-        //sprite1.p.vy = -600;
-        connection.send("UserCommand", "fire"); // keycode 90:z
-      });
-  
-      Q.input.on('action',stage,function(e) {
-        // sprite1.p.x = 500;
-        // sprite1.p.y = 100;
-        connection.send("UserCommand", "action"); // keycode 88:x
-      });
-  
-      connection.on('shapeMoved', function(x, y) {
-        //$shape.css({ left: x * 100, top: y * 100 });
-        //$x.html(x);
-        //$y.html(y);
-    
-        sprite1.p.x = x * 100;
-        sprite1.p.y = y * 100;
+    // A basic sprite shape a asset as the image
+    var sprite1 = new Q.Sprite({ x: 500, y: 100, asset: 'enemy.png', 
+                                angle: 0, collisionMask: 1, scale: 1});
+    sprite1.p.points = [
+    [ -150, -120 ],
+    [  150, -120 ],
+    [  150,   60 ],
+    [   90,  120 ],
+    [  -90,  120 ],
+    [ -150,   60 ]
+    ];
+    stage.insert(sprite1);
+    // Add the 2D component for collision detection and gravity.
+    sprite1.add('2d')
+
+    sprite1.on('step',function() {
+
+    });
+
+    // A red platform for the other sprite to land on
+    var sprite2 = new Q.Sprite({ x: 500, y: 600, w: 300, h: 200 });
+    sprite2.draw= function(ctx) {
+    ctx.fillStyle = '#FF0000';
+    ctx.fillRect(-this.p.cx,-this.p.cy,this.p.w,this.p.h);
+    };
+    stage.insert(sprite2);
+    // Bind the basic inputs to different behaviors of sprite1
+    Q.input.on('up',stage,function(e) { 
+    //sprite1.p.scale -= 0.1;
+    connection.send("UserCommand", "north"); // keycode 38:up arrow
+    });
+
+    Q.input.on('down',stage,function(e) { 
+    //sprite1.p.scale += 0.1;
+    connection.send("UserCommand", "south"); // keycode 40:down arrow
+    });
+
+    Q.input.on('left',stage,function(e) {
+    //sprite1.p.angle -= 5;
+    connection.send("UserCommand", "west"); // keycode 37:left arrow
+    });
+
+    Q.input.on('right',stage,function(e) {
+    //sprite1.p.angle += 5;
+    connection.send("UserCommand", "east"); // keycode 39:right arrow
+    });
+
+    Q.input.on('fire',stage,function(e) {
+    //sprite1.p.vy = -600;
+    connection.send("UserCommand", "fire"); // keycode 90:z
+    });
+
+    Q.input.on('action',stage,function(e) {
+    // sprite1.p.x = 500;
+    // sprite1.p.y = 100;
+    connection.send("UserCommand", "action"); // keycode 88:x
+    });
+
+    connection.on('shapeMoved', function(x, y) {
+    //$shape.css({ left: x * 100, top: y * 100 });
+    //$x.html(x);
+    //$y.html(y);
+
+    sprite1.p.x = x * 100;
+    sprite1.p.y = y * 100;
     });
     
     
