5e4f73a021e2425ee48e5b484bdf4780f2edf498
Kris Bosland <kris.bosland@puppet.com>
2018-07-25 17:01:38 -0700

    Working Phaser 3 demo

--- src/MuseWeb/wwwroot/js/example/junk.js
+++ src/MuseWeb/wwwroot/js/example/junk.js
@@ -18,11 +18,11 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
-  this.load.setBaseURL('http://labs.phaser.io');
+//   this.load.setBaseURL('http://labs.phaser.io');
 
-  this.load.image('sky', 'assets/skies/space3.png');
-  this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-  this.load.image('red', 'assets/particles/red.png');
+  this.load.image('sky', '/images/space3.png');
+  this.load.image('logo', '/images/phaser3-logo.png');
+  this.load.image('red', '/images/red.png');
 }
 
 function create ()
